58b0dd71bb88a47602b21db1aee14fbd9851e742
Yoongu Kwon <dbsrn1110@gmail.com>
2024-11-01 12:06:45 +0900

    about페이지 sec-1,2,3 관련 애니메이션 구현2 + sec-n letter 중복 수정

--- resources/js/pattern/ui-pattern-script.js
+++ resources/js/pattern/ui-pattern-script.js
@@ -40,8 +40,84 @@ $('.lang__list li').click(function () {
 //     document.querySelector('.content').style.display = 'block';
 // });
 
+// 플로그인 설치
+gsap.registerPlugin(ScrollTrigger);
+// gsap.registerPlugin(CSSRulePlugin);
+
+// 스크롤 트리거 변수 설정
+var tlS1Mt = gsap.timeline({
+    scrollTrigger: {
+        trigger: '.sec-1 .main-tit',
+        pin: true, // pin the trigger element while active
+        start: 'top 0', // when the top of the trigger hits the top of the viewport
+        end: '+=10', // end after scrolling 500px beyond the start
+        once: true, // 애니메이션을 한 번만 실행
+        toggleActions: 'play none none none', // 도착 지점에서 유지
+        scrub: 6, // smooth scrubbing, takes 1 second to "catch up" to the scrollbar
+        markers: true,
+    },
+});
+// var tlSt = gsap.timeline({
+//     scrollTrigger: {
+//         trigger: '.subtit',
+//         pin: true, // pin the trigger element while active
+//         start: 'top 0', // when the top of the trigger hits the top of the viewport
+//         end: '+=10', // end after scrolling 500px beyond the start
+//         once: true, // 애니메이션을 한 번만 실행
+//         toggleActions: 'play none none none', // 도착 지점에서 유지
+//         scrub: 6, // smooth scrubbing, takes 1 second to "catch up" to the scrollbar
+//         markers: true,
+//     },
+// });
+var tlS2Mt = gsap.timeline({
+    scrollTrigger: {
+        trigger: '.sec-2 .main-tit',
+        pin: true, // pin the trigger element while active
+        start: 'top 0', // when the top of the trigger hits the top of the viewport
+        end: '+=10', // end after scrolling 500px beyond the start
+        once: true, // 애니메이션을 한 번만 실행
+        toggleActions: 'play none none none', // 도착 지점에서 유지
+        scrub: 6, // smooth scrubbing, takes 1 second to "catch up" to the scrollbar
+        markers: true,
+    },
+});
+var tlS3Mt = gsap.timeline({
+    scrollTrigger: {
+        trigger: '.sec-3 .main-tit',
+        pin: true, // pin the trigger element while active
+        start: 'top 0', // when the top of the trigger hits the top of the viewport
+        end: '+=10', // end after scrolling 500px beyond the start
+        once: true, // 애니메이션을 한 번만 실행
+        toggleActions: 'play none none none', // 도착 지점에서 유지
+        scrub: 6, // smooth scrubbing, takes 1 second to "catch up" to the scrollbar
+        markers: true,
+    },
+});
+
 // about페이지 section1 관련
-gsap.from('.sec-1__letter1', { opacity: 0, duration: 2 });
-gsap.from('.sec-1__letter3', { opacity: 0, duration: 2, delay: 1 });
-gsap.from('.sec-1__letter2', { opacity: 0, x: -200, duration: 1, delay: 2 });
-gsap.from('.sec-1__letter4', { opacity: 0, x: -200, duration: 1, delay: 3 });
+// title
+tlS1Mt
+    .from('.sec-1__letter1', { opacity: 0, duration: 1 })
+    .from('.sec-1__letter3', { opacity: 0, duration: 1 })
+    .from('.sec-1__letter2', { opacity: -4, x: -200, duration: 1 })
+    .from('.sec-1__letter4', { opacity: -4, x: -200, duration: 1 })
+    .from('.sec-1 .subtit', { opacity: -4, y: -100, duration: 1 });
+
+// main01-list
+// tlSt.from('.num1, .title1', { opacity: -2, duration: 1 })
+//     .from(cssRulePlugin.getRule('.img-wrap::after'), { opacity: -2, x: -200, duration: 1 })
+//     .from('.bike-oj-1', { opacity: 0, y: 200, duration: 1 });
+
+tlS2Mt
+    .from('.sec-2__letter1', { opacity: 0, duration: 1 })
+    .from('.sec-2__letter3', { opacity: 0, duration: 1 })
+    .from('.sec-2__letter2', { opacity: -4, x: -200, duration: 1 })
+    .from('.sec-2__letter4', { opacity: -4, x: -200, duration: 1 })
+    .from('.sec-2 .subtit', { opacity: -4, y: -100, duration: 1 });
+
+tlS3Mt
+    .from('.sec-3__letter1', { opacity: 0, duration: 1 })
+    .from('.sec-3__letter3', { opacity: 0, duration: 1 })
+    .from('.sec-3__letter2', { opacity: -4, x: -200, duration: 1 })
+    .from('.sec-3__letter4', { opacity: -4, x: -200, duration: 1 })
+    .from('.sec-3 .subtit', { opacity: -4, y: -100, duration: 1 });
