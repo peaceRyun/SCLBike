e80a8fc4c70c61af88e1838e622b16bddb6fb6cd
dbsrn1110 <dbsrn1110@gmail.com>
2024-11-09 17:04:58 +0900

    about페이지 플레이정지버튼 (추가 + 위치 변경)

--- resources/js/pattern/ui-slick-script.js
+++ resources/js/pattern/ui-slick-script.js
@@ -8,6 +8,26 @@ $('.fade').slick({
     arrows: false,
 });
 
+$('.intro-slide').on('afterChange', function (event, slick, currentSlide) {
+    const progress = ((currentSlide + 1) / slick.slideCount) * 100;
+    $('.progress-bar').css('width', progress + '%');
+});
+
+// 초기 상태 설정
+let isPlaying = true;
+
+$('#play-pause-btn').on('click', function () {
+    if (isPlaying) {
+        $('.intro-slide').slick('slickPause');
+        $(this).removeClass('pause').addClass('play');
+    } else {
+        $('.intro-slide').slick('slickPlay');
+        $(this).removeClass('play').addClass('pause');
+    }
+    // 상태 토글
+    isPlaying = !isPlaying;
+});
+
 $('.single').slick({
     dots: true,
     infinite: false,
